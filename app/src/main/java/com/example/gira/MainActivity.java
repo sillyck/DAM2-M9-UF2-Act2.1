5453af403a7437deea1f820ab72b8302ac0bab8c
EloySPAIN <yole.tres@gmail.com>
2023-02-14 15:18:34 +0100

    mapa implementado

--- app/src/main/java/com/example/gira/MainActivity.java
+++ app/src/main/java/com/example/gira/MainActivity.java
@@ -2,13 +2,16 @@ package com.example.gira;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.mapa);
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
     }
 }
