9c7e99599e2cd2376dff732f92a10e7df1955895
Jordi Ribellas <ribellas2002@gmail.com>
2023-02-25 14:14:39 +0100

    a mitjes

--- app/src/main/java/com/example/gira/MapsActivity.java
+++ app/src/main/java/com/example/gira/MapsActivity.java
@@ -2,7 +2,6 @@ package com.example.gira;
 
 import androidx.fragment.app.FragmentActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
@@ -25,11 +24,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-//        binding = MapaBinding.inflate(getLayoutInflater());
-//        setContentView(binding.getRoot());
         setContentView(R.layout.mapa);
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -41,15 +37,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         });
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         nMapView = googleMap;
@@ -84,8 +71,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     private void openPantalla(Class clase) {
-//        Intent intent = new Intent(this, clase);
-//        startActivity(intent);
         finish();
     }
 }
